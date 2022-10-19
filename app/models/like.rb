805308733056e63d28eed36c342dc6dbd1192837
Diego Yon <diegoyon@outlook.com>
2022-10-18 21:03:03 -0600

    add after save

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,10 @@
 class Like < ApplicationRecord
+  after_save :update_likes_counter
+  
   belongs_to :author
   belongs_to :post
+
+  private
 
   def update_likes_counter
     post.update(likes_counter: post.likes.length)
