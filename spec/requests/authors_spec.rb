4933ec5dad3851844a180fd6311e6879ef46958a
Diego Yon <diegoyon@outlook.com>
2022-10-25 13:38:14 -0600

    add specs for authors

--- spec/requests/authors_spec.rb
+++ spec/requests/authors_spec.rb
@@ -2,6 +2,20 @@ require 'rails_helper'
 
 RSpec.describe 'Authors', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+
+    it "returns a 200 OK status" do
+      get "/authors"
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "checks if the correct themplate was rendered" do
+      get "/authors"
+      expect(response).to render_template(:index)
+    end
+
+    it "checks if the response body includes correct placeholder text" do
+      get "/authors"
+      expect(response.body).to include("HERE IS THE LIST OF AUTHORS")
+    end
   end
 end
