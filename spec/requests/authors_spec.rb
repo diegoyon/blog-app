966010ca246e5c588ef12bf1d7c9489eac67e274
Diego Yon <diegoyon@outlook.com>
2022-10-25 13:45:01 -0600

    fix linters

--- spec/requests/authors_spec.rb
+++ spec/requests/authors_spec.rb
@@ -2,20 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'Authors', type: :request do
   describe 'GET /index' do
-
-    it "returns a 200 OK status" do
-      get "/authors"
+    it 'returns a 200 OK status' do
+      get '/authors'
       expect(response).to have_http_status(:ok)
     end
 
-    it "checks if the correct themplate was rendered" do
-      get "/authors"
+    it 'checks if the correct themplate was rendered' do
+      get '/authors'
       expect(response).to render_template(:index)
     end
 
-    it "checks if the response body includes correct placeholder text" do
-      get "/authors"
-      expect(response.body).to include("HERE IS THE LIST OF AUTHORS")
+    it 'checks if the response body includes correct placeholder text' do
+      get '/authors'
+      expect(response.body).to include('HERE IS THE LIST OF AUTHORS')
     end
   end
 end
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,19 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    it "returns a 200 OK status" do
-      get "/authors/9/posts"
+    it 'returns a 200 OK status' do
+      get '/authors/9/posts'
       expect(response).to have_http_status(:ok)
     end
 
-    it "checks if the correct themplate was rendered" do
-      get "/authors/9/posts"
+    it 'checks if the correct themplate was rendered' do
+      get '/authors/9/posts'
       expect(response).to render_template(:index)
     end
 
-    it "checks if the response body includes correct placeholder text" do
-      get "/authors/9/posts"
-      expect(response.body).to include("HERE IS THE LIST OF POSTS FOR A GIVEN AUTHOR")
+    it 'checks if the response body includes correct placeholder text' do
+      get '/authors/9/posts'
+      expect(response.body).to include('HERE IS THE LIST OF POSTS FOR A GIVEN AUTHOR')
     end
   end
 end
