746601ba6e854cba53359ed8ea8e591f12e07be5
Diego Yon <diegoyon@outlook.com>
2022-10-27 22:19:50 -0600

    Add functionality to add comments on behalf of the current_user

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,20 @@
 class CommentsController < ApplicationController
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @post = Post.find_by(id: params[:post_id])
+    @comment = Comment.new(
+      text: params[:comment][:text],
+      post_id: @post.id,
+      author_id: current_user.id
+    )
+    if @comment.save
+      redirect_to author_post_path(current_user, @post)
+    else
+      render :new
+    end
+  end
 end
