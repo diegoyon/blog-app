d2c235fe07700bf587eb0087e0d7c4ba431df917
Diego Yon <diegoyon@outlook.com>
2022-10-31 16:03:43 -0600

    add author path

--- spec/features/author_show_spec.rb
+++ spec/features/author_show_spec.rb
@@ -12,46 +12,46 @@ RSpec.describe 'Author show page', type: :system do
     Post.create!(author: @author2, title: 'Post 2', text: 'text Post 2', comments_counter: 0, likes_counter: 0)
     Post.create!(author: @author2, title: 'Post 3', text: 'text Post 3', comments_counter: 0, likes_counter: 0)
     
-                              visit author_path
+                              visit author_path(@author2)
   end
   describe 'show page' do
     it 'see the user\'s profile picture.' do
-      visit author_path
+      visit author_path(@author2)
       image = page('img')
       expect(image.src).to eq('https://picsum.photos/200')
     end
     it 'see the user\'s username' do
-      visit author_path
+      visit author_path(@author2)
       expect(page).to have_content('Diego')
     end
     it 'see the number of posts the user has written.' do
-      visit author_path
+      visit author_path(@author2)
       expect(page).to have_content('Number of posts: 3')
     end
     it 'see the user\'s bio' do
-      visit author_path
+      visit author_path(@author2)
       expect(page).to have_content('Engineer from Guatemala')
     end
     it 'see the user\'s first 3 posts.' do
-      visit author_path
+      visit author_path(@author2)
       posts = page.all('.author-post')
       expect(posts.count).to eq(3)
     end
 
     it 'see a button that lets me view all of a user\'s posts.' do
-      visit author_path
+      visit author_path(@author2)
       button = page.find('button')
       expect(button.value).to eq('See All Posts')
     end
 
     it 'When I click a user\'s post, it redirects me to that post\'s show page.' do
-      visit author_path
+      visit author_path(@author2)
       click_on('Post 1')
       expect(page).to have_content('Write a comment...')
     end
 
     it 'When I click to see all posts, it redirects me to the user\'s post\'s index page.' do
-      visit author_path
+      visit author_path(@author2)
       click_on('See All Posts')
       posts = page.all('.author-post')
       expect(posts.count).to eq(Author.posts.all.count)
