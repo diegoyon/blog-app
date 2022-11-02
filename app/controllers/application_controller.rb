16d79da91a6313473604c77868076312afbfb274
Diego Yon <diegoyon@outlook.com>
2022-11-01 18:24:15 -0600

    Change default parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :bio, :photo, :posts_counter])
+    devise_parameter_sanitizer
+      .permit(:sign_up, keys: [:name, :posts_counter])
   end
 end
