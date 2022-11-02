31aeac30f41f2a9aa52ea6efa1de9d8828f872da
Diego Yon <diegoyon@outlook.com>
2022-11-01 18:08:36 -0600

    Add permitted parameteres

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_author!
   def current_user
     Author.first
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :bio, :photo, :posts_counter])
   end
 end
