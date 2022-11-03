5287fe6ccf27a88f6c24464b0a591b0dcd4ac593
Diego Yon <diegoyon@outlook.com>
2022-11-03 11:56:23 -0600

    remove csrf

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  load_and_authorize_resource
+  # load_and_authorize_resource
   def index
     @author = Author.find(params[:author_id])
     @like = Like.new
