a2fe55b936e9f04a90adf21b24da34563572e8ee
Nyame-Wolf <mumenyam@gmail.com>
2022-11-04 00:02:19 +0300

    Add api routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  # web routes
   root "authors#index"
   post '/auth/login', to: 'authentication#login'
   resources :authors do
@@ -12,3 +13,14 @@ Rails.application.routes.draw do
     end
   end
 end
+
+# API ROUTES
+  namespace :api do # /api
+    namespace :v1 do # /api/v1
+      resources :users do # api/v1/users
+        resources :posts, format: :json do # api/v1/users/:user_id/posts
+          resources :comments, format: :json # api/v1/users/:user_id/posts/:post_id/comments
+        end
+      end
+    end
+  end
