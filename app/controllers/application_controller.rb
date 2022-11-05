e22e12382b5fed89e68e0cdd3e51e5112d0dc04a
Diego Yon <diegoyon@outlook.com>
2022-11-05 12:07:22 -0600

    Add edit profile feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer
-      .permit(:sign_up, keys: %i[name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name bio photo])
   end
 end
